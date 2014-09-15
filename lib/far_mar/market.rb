daf7acb3d24b98cab2a9aac2813d8c1849501429
Kristen McCabe <kmccabe3@gmail.com>
2014-09-15 16:01:49 -0700

    Add self.all

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,4 +1,35 @@
 module FarMar
   class Market
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
+
+    def initialize
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
+      @vendors = []
+    end
+
+    # Execute by typing FarMar::Market.all
+    def self.all
+      CSV.read("support/markets.csv")
+    end
+
+    # Execute by typing FarMar::Market.find(id)
+    def self.find(id)
+      file = CSV.read("support/market.csv")
+      file.first[0].each do | |
+
+      [first..last].each do |number|
+        a[number][0] == id
+      end
+    end
+
   end
 end
+
+
+FarMar::Market.new
