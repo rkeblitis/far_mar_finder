dd15b381a540df6d394b21fe09ce4b79c7739319
Kristen McCabe <kmccabe3@gmail.com>
2014-09-16 12:43:47 -0700

    More Market class work

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,39 +1,23 @@
 module FarMar
   class Market
-    attr_accessor :id, :name, :address, :city, :county, :state, :zip
 
-    def initialize (market_index)
-      @id       = market_index[0]
-      @name     = market_index[1]
-      @address  = market_index[2]
-      @city     = market_index[3]
-      @county   = market_index[4]
-      @state    = market_index[5]
-      @zip      = market_index[-1]
-# instanciate all markets here
+    def initialize(row)
+      @id = row[0]
+      @name = row[1]
+      @address = row[2]
+      @city = row[3]
+      @county = row[4]
+      @state = row[5]
+      @zip = row[6]
     end
 
-    # Execute by typing FarMar::Market.all
-    def self.all # transform every individual array into an instance
-      # the values within the individuals arrays will should be the instance
-      # variables.
-      @market_index = CSV.read("support/markets.csv")
+    #reads in the CSV market data as an array of arrays
+    #creates a new array filled with all market objects
+    def self.all
+      market_data = CSV.read("support/markets.csv")
+      market_data.map { |row| FarMar::Market.new(row) }
     end
-
-    # Execute by typing FarMar::Market.find(id)
-    def self.find(id)
-      file = CSV.read("support/market.csv")
-      #returns the row where the ID field matches the argument
-
-      # convert into market objects
-      a = file.find { |array| array[0] == id}
-      FarMar::Market.new(a[0])
-
-
-
-    end
-
-
 
   end
+
 end
