7449d5e0a9b0ae7752a7d5bdefdc40a461a9062a
Kristen McCabe <kmccabe3@gmail.com>
2014-09-17 18:35:22 -0700

    Add final method in Bronze level

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -5,7 +5,7 @@ module FarMar
     def initialize(sale_row)
       @id            = sale_row[0].to_i
       @amount        = sale_row[1].to_i #(in cents)
-      @purchase_time = sale_row[2]
+      @purchase_time = Time.parse(sale_row[2]) # right format? 2013-11-07 04:34:56 -0800 
       @vendor_id     = sale_row[3].to_i
       @product_id    = sale_row[4].to_i
     end
@@ -27,19 +27,19 @@ module FarMar
     #returns the FarMar::Vendor instance that is associated
     #with this sale using the FarMar::Sale vendor_id field
     def vendor
-      FarMar::Vendor.all.find_all { |vendor| vendor.id == @vendor_id }
+      FarMar::Vendor.all.find { |vendor| vendor.id == @vendor_id }
     end
 
     #returns the FarMar::Product instance that is associated with
     #this sale using the FarMar::Sale product_id field
     def product
-      FarMar::Product.all.find_all { |product| product.id == @product_id }
+      FarMar::Product.all.find { |product| product.id == @product_id }
     end
 
     #returns a collection of FarMar::Sale objects where the purchase time is
     #between the two times given as arguments
     def self.between(beginning_time, end_time)
-      self.all
+      self.all.find_all { |sale| sale.between?(beginning_time, end_time) }
     end
 
   end
